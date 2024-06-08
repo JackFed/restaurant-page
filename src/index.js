81a27c273f6357c3efec5b4ea99334d4bf62ca0e
JackFed <jack.fedurek@gmail.com>
2024-06-08 16:15:55 -0400

    Add basic header layout in CSS

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
+import './styles.css';
+import Icon from './images/beef-kabob.jpg';
+
 function component() {
-    const buttons = document.querySelectorAll('button');
-    console.log(buttons);
+  const element = document.createElement('div');
+  const images = document.querySelector('img');
+  images.classList.add("food-image");
+  console.log(images);
+
+ // Add the image to our existing div.
+ const myIcon = new Image();
+ myIcon.src = Icon;
+
+ element.appendChild(myIcon);
+
+  return element;
 }
 
-component();
+document.body.appendChild(component());
